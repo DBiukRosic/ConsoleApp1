8ca6a87ed797fdd23ffaab1751a53f4c02ed38a5
JBarti <josip.bartulovic3@skole.hr>
2022-03-15 17:02:51 +0100

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.write("Izbrisao sam ti sav kod MUAHAHAHAH");
+            Console.Write("Izbrisao sam ti sav kod MUAHAHAHAH");
         }
     }
 }
